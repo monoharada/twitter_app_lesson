f293c6cb8b50934be992d95f3e1bddb129f61a03
monoharada <harada@monosus.co.jp>
2021-06-03 08:13:22 +0000

    l-19

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -48,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
   },
   image: {
     backgroundImage:
-      "url(https://images.unsplash.com/photo-1581784368651-8916092072cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80)",
+      "url(https://images.unsplash.com/photo-1568819317551-31051b37f69f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { auth } from "../firebase";
+import TweetInput from './TweetInput';
 
 const Feed = () => {
   return (
     <div>
-      Feed!!!!!!
-      <button onClick={()=>auth.signOut()}>Logout</button>
+      <TweetInput />
+      <button onClick={() => auth.signOut()}>Logout</button>
     </div>
   )
 }
