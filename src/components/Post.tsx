b880b4c01cf2093ff5ed70f800a51c4c08de562c
monoharada <harada@monosus.co.jp>
2021-06-06 00:15:19 +0000

    l-24

---
+++ src/components/Post.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import styles from './Post.module.css'
+import { db } from '../firebase'
+import firebase from 'firebase/app'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/userSlice'
+import { Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+import MessageIcon from '@material-ui/icons/Message'
+import SendIcon from '@material-ui/icons/Send'
+
+interface PROPS {
+  postId: string
+  avatar: string
+  image: string
+  text: string
+  timestamp: any
+  username: string
+}
+const Post: React.FC<PROPS> = (props) => {
+  const user = useSelector(selectUser)
+  const [comment, setComment] = useState('')
+  const newComment = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    db.collection('posts').doc(props.postId).collection('comments').add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: user.displayName,
+    })
+    setComment('')
+  }
+  return (
+    <div className={styles.post}>
+      <div className={styles.post_avatar}>
+        <Avatar src={props.avatar} />
+      </div>
+      <div className={styles.post_body}>
+        <div>
+          <div className={styles.post_header}>
+            <h3>
+              <span className={styles.post_headerUser}>@{props.username}</span>
+              <span className={styles.post_headerTime}>
+                {new Date(props.timestamp?.toDate()).toLocaleString()}
+              </span>
+            </h3>
+          </div>
+          <div className={styles.post_tweet}>
+            <p>{props.text}</p>
+          </div>
+        </div>
+        {props.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={props.image} alt="tweet" />
+          </div>
+        )}
+        <form onSubmit={newComment}>
+          <div className={styles.post_form}>
+            <input
+              type="text"
+              className={styles.post_input}
+              placeholder="Type new comment"
+              value={comment}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setComment(e.target.value)}
+            />
+            <button
+              disabled={!comment}
+              className={comment ? styles.post_button : styles.post_buttonDisable}
+              type="submit"
+            >
+              <SendIcon className={styles.post_sendIcon} />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Post
